f48479ea0b5f7c0a8a71eea426aa455278de651b
madKoding <madkoding@gmail.com>
2025-08-15 20:06:41 -0400

    Remove unused board configurations and clean up project
    - Remove all slimenrf_r1, slimenrf_r2, slimenrf_r3 board configurations
    - Remove nrf52dk, nrf52840dk, nrf21540dk, nrf5340dk board configurations
    - Remove mochi and other unused board configurations
    - Remove corresponding pm_static configuration files
    - Clean up board references in workflow.yml to only support promicro_uf2/nrf52840/spi
    - Remove board references in src/globals.h and src/build_defines.h
    - Update sample.yaml to reflect only supported boards
    - Simplify project structure to focus on single board configuration

--- src/globals.h
+++ src/globals.h
@@ -27,49 +27,36 @@
 
 #include "retained.h"
 
-#define USER_SHUTDOWN_ENABLED CONFIG_USER_SHUTDOWN // Allow user to use reset or sw0 to shutdown
-#define MAG_ENABLED CONFIG_SENSOR_USE_MAG // Use magnetometer if it is present
-#define IGNORE_RESET CONFIG_IGNORE_RESET // If sw0 available, don't change any reset behavior
-#define WOM_USE_DCDC CONFIG_WOM_USE_DCDC // Use DCDC instead of LDO for WOM if it is more efficient
+#define USER_SHUTDOWN_ENABLED \
+	CONFIG_USER_SHUTDOWN  // Allow user to use reset or sw0 to shutdown
+#define MAG_ENABLED CONFIG_SENSOR_USE_MAG  // Use magnetometer if it is present
+#define IGNORE_RESET \
+	CONFIG_IGNORE_RESET  // If sw0 available, don't change any reset behavior
+#define WOM_USE_DCDC \
+	CONFIG_WOM_USE_DCDC  // Use DCDC instead of LDO for WOM if it is more efficient
 
-/* Sensor gyroscope, accelerometer, and magnetometer axes should align to the IMU body axes
- * SENSOR_QUATERNION_CORRECTION should align the sensor to the device following Android convention
- * On flat surface / face up:
- * Left from the perspective of the device / right from your perspective is +X
- * Front side (facing up) is +Z
- * Mounted on body / standing up:
- * Top side of the device is +Y
- * Front side (facing out) is +Z
+/* Sensor gyroscope, accelerometer, and magnetometer axes should align to the IMU body
+ * axes SENSOR_QUATERNION_CORRECTION should align the sensor to the device following
+ * Android convention On flat surface / face up: Left from the perspective of the device
+ * / right from your perspective is +X Front side (facing up) is +Z Mounted on body /
+ * standing up: Top side of the device is +Y Front side (facing out) is +Z
  */
 
-#if defined(CONFIG_BOARD_SLIMEVRMINI_P1_UF2) || defined(CONFIG_BOARD_SLIMEVRMINI_P2_UF2)
-#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gy, -gz, -gx
-#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ay, -az, -ax
-#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT -mx, mz, -my
-#define SENSOR_QUATERNION_CORRECTION 0.7071f, 0.7071f, 0.0f, 0.0f
-#endif
-#if defined(CONFIG_BOARD_SLIMEVRMINI_P4_UF2)
-#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gx, gy, gz
-#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ax, ay, az
-#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, -mx, -mz
-#define SENSOR_QUATERNION_CORRECTION 0.7071f, 0.0f, 0.0f, 0.7071f 
-#endif
-
-#if defined(CONFIG_BOARD_SLIMENRF_R1) || defined(CONFIG_BOARD_SLIMENRF_R2) || defined(CONFIG_BOARD_SLIMENRF_R3)
-#define SENSOR_QUATERNION_CORRECTION 0.0f, 0.7071f, 0.7071f, 0.0f
-#endif
-
 #ifndef SENSOR_GYROSCOPE_AXES_ALIGNMENT
-#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gx, gy, gz // gyro axes alignment to sensor body
+#define SENSOR_GYROSCOPE_AXES_ALIGNMENT \
+	gx, gy, gz  // gyro axes alignment to sensor body
 #endif
 #ifndef SENSOR_ACCELEROMETER_AXES_ALIGNMENT
-#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ax, ay, az // accel axes alignment to sensor body
+#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT \
+	ax, ay, az  // accel axes alignment to sensor body
 #endif
 #ifndef SENSOR_MAGNETOMETER_AXES_ALIGNMENT
-#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, -mx, -mz // mag axes alignment to sensor body
+#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT \
+	my, -mx, -mz  // mag axes alignment to sensor body
 #endif
 #ifndef SENSOR_QUATERNION_CORRECTION
-#define SENSOR_QUATERNION_CORRECTION 1.0f, 0.0f, 0.0f, 0.0f // correction quat for sensor to mounting orientation
+#define SENSOR_QUATERNION_CORRECTION \
+	1.0f, 0.0f, 0.0f, 0.0f  // correction quat for sensor to mounting orientation
 #endif
 
 #endif
