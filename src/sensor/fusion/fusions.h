be698de8d51541f6e5e507933334776dd6bf4845
madKoding <madkoding@gmail.com>
2025-08-24 10:50:17 -0400

    Add EKF sensor fusion option

--- src/sensor/fusion/fusions.h
+++ src/sensor/fusion/fusions.h
@@ -23,28 +23,20 @@
 #ifndef SLIMENRF_SENSOR_FUSIONS
 #define SLIMENRF_SENSOR_FUSIONS
 
+#include "ekf/ekf.h"
 #include "motionsense/motionsense.h"
-#include "xiofusion/xiofusion.h"
 #include "vqf/vqf.h"
+#include "xiofusion/xiofusion.h"
 
-enum fusion {
-    FUSION_NONE,
-    FUSION_FUSION,
-    FUSION_MOTIONSENSE,
-	FUSION_VQF
-};
+enum fusion { FUSION_NONE, FUSION_FUSION, FUSION_MOTIONSENSE, FUSION_VQF, FUSION_EKF };
 
-const char *fusion_names[] = {
-    "None",
-    "x-io Technologies Fusion",
-    "NXP SensorFusion",
-    "VQF"
-};
-const sensor_fusion_t *sensor_fusions[] = {
-    NULL,
-    &sensor_fusion_fusion,
-    &sensor_fusion_motionsense,
-    &sensor_fusion_vqf
-};
+const char* fusion_names[]
+	= {"None", "x-io Technologies Fusion", "NXP SensorFusion", "VQF", "EKF"};
+const sensor_fusion_t* sensor_fusions[]
+	= {NULL,
+	   &sensor_fusion_fusion,
+	   &sensor_fusion_motionsense,
+	   &sensor_fusion_vqf,
+	   &sensor_fusion_ekf};
 
 #endif
